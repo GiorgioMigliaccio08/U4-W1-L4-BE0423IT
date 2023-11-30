6d46ec3d9256c49b48988aeefdd6833fa75e6766
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-30 15:51:45 +0100

    Primo Commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Invio with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package entities;
 
-        // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class Dipendente {
+    public int stipendioBase= 1000;
+    public int Matricola;
+    private  int stipendio;
+    private int importoOrarioStraordinario;
+    private String Livello;
+    private String Dipartimento;
 
-            // Press Maiusc+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    public Dipendente ( int stipendioBase, int Matricola, int stipendio, int importoOrarioStraordinario, String Livello, String Dipartimento){
+        this.stipendioBase = stipendioBase;
+        this.Matricola = Matricola;
+        this.stipendio = stipendio;
+        this.importoOrarioStraordinario= importoOrarioStraordinario;
+        this.Livello = Livello;
+        this.Dipartimento = Dipartimento;
+
+    }
+
+    //METODI
+    public void nomiunativoDipendente() {
+        System.out.println("La mia matricola è " + this.Matricola + " ed il mio stipendio è " + this.stipendio + " euro.");
     }
 }
